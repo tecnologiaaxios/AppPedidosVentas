9342206424dd7aedadfc4eea2a521913601d3492
Saul Vega <saulaxios@gmail.com>
2017-08-15 09:46:48 -0500

    Notificaciones en al app de nuevo.

--- www/js/pedidos.js
+++ www/js/pedidos.js
@@ -6,12 +6,59 @@ function logout() {
   auth.signOut();
 }
 
+function mostrarNotificaciones() {
+  let usuario = auth.currentUser.uid;
+  let notificacionesRef = db.ref('notificaciones/tiendas/'+usuario+'/lista');
+  notificacionesRef.on('value', function(snapshot) {
+    let lista = snapshot.val();
+    let trs = "";
+
+    let arrayNotificaciones = [];
+    for(let notificacion in lista) {
+      arrayNotificaciones.push(lista[notificacion]);
+    }
+
+    for(let i in arrayNotificaciones) {
+      let date = arrayNotificaciones[i].fecha;
+      moment.locale('es');
+      let fecha = moment(date, "MMMM DD YYYY, HH:mm:ss").fromNow();
+
+      trs += '<tr><td>'+arrayNotificaciones[i].mensaje +' '+fecha+'</td></tr>'
+    }
+
+    $('#notificaciones').html(trs);
+  });
+}
+
+function mostrarContador() {
+  let uid = auth.currentUser.uid;
+  let notificacionesRef = db.ref('notificaciones/tiendas/'+uid);
+  notificacionesRef.on('value', function(snapshot) {
+    let cont = snapshot.val().cont;
+
+    if(cont > 0) {
+      $('#spanNotificaciones').html(cont).show();
+    }
+    else {
+      $('#spanNotificaciones').hide();
+    }
+  });
+}
+
+function verNotificaciones() {
+  let uid = auth.currentUser.uid;
+  let notificacionesRef = db.ref('notificaciones/tiendas/'+uid);
+  notificacionesRef.update({cont: 0});
+}
+
 function haySesion() {
   auth.onAuthStateChanged(function (user) {
     //si hay un usuario
     if (user) {
       llenarSelectTiendas();
       mostrarHistorialPedidos();
+      mostrarNotificaciones();
+      mostrarContador();
     }
     else {
       $(location).attr("href", "index.html");
